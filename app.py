d5978c50b24980cd6e88b9558d3ee43670d39e1e
ambresh018 <ambresh.lingadhar@dataeaze.io>
2023-09-03 17:14:17 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+from langchain.llms import OpenAI
+import streamlit as st
+
+os.environ["OPENAI_API_KEY"] = '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' # ${{ secrets.OPENAIKEY }}
+
+# streamlit framework
+st.title('**Ambresh Search Results**')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    input_text=st.text_input("**Search the topic you want**")
+
+with col2:
+    temp = st.slider('**Select Temperature**', 0.1, 1.0)
+
+## OPENAI LLMS
+llm=OpenAI(temperature=temp)
+
+if input_text:
+    st.write(llm(input_text))
+
